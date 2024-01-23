76966fe2df512a562eb65f9f6d311f9b35ab4cfa
Efrain Enrique Guzman Cardenas <efrain.guzman@FZCORP.local>
2024-01-23 14:57:10 -0500

    maquetacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+       <header id="root">
+        <div className="logo-container">
+          <a  target="_blank">
+            <img src='/public/images.png' className="logo"  />
+          </a>
+        </div>
+        </header>
+        
+        <nav>
+          <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Vehículos</a></li>
+            <li><a href="#">Ayuda</a></li>
+            <li><a href="#">Contacto</a></li>
+            <li><a href="#">Políticas</a></li>
+          </ul>
+        </nav>
+      
+
+
+      <section>
+        <h1>+</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </section>
+
+
+
+      <footer>
+        <p>&copy; 2024 Tu Página Dinámica. Todos los derechos reservados.</p>
+      </footer>
     </>
   )
 }
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App
